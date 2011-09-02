4a00b3def0cd561567e54a1851764e9eea8b052e
hatch2 <hiroaki@hatch2.com>
2011-09-02 11:03:53 +0900

    Update button action, Update app icon.

--- ux/uxAppDelegate.m
+++ ux/uxAppDelegate.m
@@ -16,10 +16,15 @@
 
 - (IBAction)shorturlBTNA:(id)sender {
     [longtext resignFirstResponder];
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://ux.nu/api/short?url=%@&format=plain",longtext.text]]];
+    NSData *response = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+    NSString *json_string = [[NSString alloc] initWithData:response encoding:NSUTF8StringEncoding];
+    
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString: [NSString stringWithFormat:@"twitter://post?message=%@",json_string]]];
 }
 
 - (IBAction)longurlboxOUTA:(id)sender {
-    [longtext resignFirstResponder];
+    //[longtext resignFirstResponder];
 }
 
 - (IBAction)infoBTNA:(id)sender {
@@ -30,6 +35,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    [longtext becomeFirstResponder];
     [self.window makeKeyAndVisible];
     return YES;
 }
@@ -50,6 +56,8 @@
 
 - (void)applicationWillResignActive:(UIApplication *)application
 {
+    
+    [longtext becomeFirstResponder];
     /*
      Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
      Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
@@ -58,6 +66,7 @@
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
+    [longtext becomeFirstResponder];
     /*
      Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
      If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
@@ -66,6 +75,8 @@
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
+    
+    [longtext becomeFirstResponder];
     /*
      Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
      */
@@ -73,6 +84,8 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
+    
+    [longtext becomeFirstResponder];
     /*
      Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
      */
@@ -80,6 +93,8 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
+    
+    [longtext becomeFirstResponder];
     /*
      Called when the application is about to terminate.
      Save data if appropriate.
